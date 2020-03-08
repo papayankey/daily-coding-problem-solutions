aa575ed808e64c4b44b869fc2d03280f646f2782
iambenbright <yankeybenneth@gmail.com>
2020-03-08 13:35:35 +0000

    added 2020/03/07 problem_with_NO SOLUTION YET

---
+++ March 2020/2020_03_07.js
@@ -0,0 +1,26 @@
+// This problem was asked by Google.
+
+// Given the root to a binary tree,
+// implement serialize(root),
+// which serializes the tree into a string, and
+// deserialize(s), which deserializes the string
+// back into the tree.
+
+// For example, given the following Node class
+
+/*
+    class Node {
+      constructor(val, left=null, rigt=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+      }
+    }
+ */
+
+// The following test should pass:
+
+/* 
+  node = Node('root', Node('left', Node('left.left')), Node('right'))
+  assert deserialize(serialize(node)).left.left.val == 'left.left'
+*/
