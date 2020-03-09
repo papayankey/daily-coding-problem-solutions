8fd2eea125bfe1cbb74625bd4d31eee1d1a9d43a
iambenbright <yankeybenneth@gmail.com>
2020-03-09 12:35:25 +0000

    added solution to 2020/03/07 problem

--- March 2020/2020_03_07.js
+++ March 2020/2020_03_07.js
@@ -1,3 +1,5 @@
+const assert = require('assert').strict;
+
 // This problem was asked by Google.
 
 // Given the root to a binary tree,
@@ -28,3 +30,46 @@
 // ===
 
 // Solution
+
+// tree node
+class Node {
+  constructor(val, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+// serialize fn
+function serialize(node) {
+  // json stringify node
+  let json = JSON.stringify(node);
+  return json;
+}
+
+// deserialize fn
+function deserialize(json) {
+  // json parse stringified node to object
+  let node = JSON.parse(json);
+  return node;
+}
+
+// test
+let node = new Node(
+  'root',
+  new Node('left', new Node('left.left')),
+  new Node('right'),
+);
+
+try {
+  let actual = node.left.left.val;
+  let expected = 'left.left';
+  assert.deepStrictEqual(actual, expected);
+  console.log(`
+  Values are strictly deep-equal: 
+    + actual: ${actual}
+    + expected: ${expected}
+  `);
+} catch (error) {
+  console.log(error.message);
+}
